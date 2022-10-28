78c2774646ba29db16305782df42f7b65c9bb928
Ming Liang <2540341431@qq.com>
2022-10-28 15:42:01 +0800

    add icon into package

--- Language Learning Winform Entry 2/Properties/Resources.Designer.cs
+++ Language Learning Winform Entry 2/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Language_Learning_Winform.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   查找类似于 (图标) 的 System.Drawing.Icon 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Icon BOOK {
+            get {
+                object obj = ResourceManager.GetObject("BOOK", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
